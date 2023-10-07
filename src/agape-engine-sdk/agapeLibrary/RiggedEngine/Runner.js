7068839f83e488af7c4011fc770d3d47df371918
Wong Lok <wonglok@users.noreply.github.com>
2023-10-07 11:33:43 +0800

    runner

--- src/agape-engine-sdk/agapeLibrary/RiggedEngine/Runner.js
+++ src/agape-engine-sdk/agapeLibrary/RiggedEngine/Runner.js
@@ -261,7 +261,7 @@ class Display extends Object3D {
 
           float t = o_move.a + o_pos.a + rand(vMyUV.xy);
           // vec3 myColor = 1.0 * pal(time + o_pos.a + o_move.a + abs(o_move.x * 0.005 * -cos(3.0 * time)), vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,0.0,0.5),vec3(0.8,0.90,0.30));
-          vec3 myColor = cosPalette(t,vec3(0.21,0.55,0.63),vec3(0.2,0.5,0.33),vec3(0.2,0.18,0.75),vec3(0.06,0.16,0.65));
+          vec3 myColor = cosPalette(t,vec3(0.63, 0.55, 0.21),vec3(0.5,0.2,0.33),vec3(0.18,0.75,0.6),vec3(0.65, 0.06,0.16));
 
           if (rand(vMyUV.xy) <= 0.015) {
             myColor += 35.0 * (myColor);
